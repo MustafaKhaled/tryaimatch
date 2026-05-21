0f259274c618e435913dfe91fff3c5df5e9d724a
Mustafa Khaled <mustafakhaledmustafa@outlook.com>
2026-05-21 03:56:43 +0200

    Add Google Tag Manager scripts for enhanced tracking and analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,6 +13,15 @@ export default defineNuxtConfig({
         { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Caveat:wght@500;700&family=IBM+Plex+Sans:wght@400;500;600;700&display=swap' },
         { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@3.5.0/tabler-icons.min.css' }
+      ],
+        script: [
+        {
+          async: true,
+          src: 'https://www.googletagmanager.com/gtag/js?id=G-HRG7JVJRMS'
+        },
+        {
+          innerHTML: "window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-HRG7JVJRMS');"
+        }
       ]
     }
   },
